bfa9b91f7361266f89ad5f0646408881af132aef
Abdullaaurad <abdullaaurad@gmail.com>
2024-04-13 02:33:12 +0530

    1st

---
+++ Basic/print2.java
@@ -0,0 +1,10 @@
+public class print2{                            // class suppose to have the same name as the file it's in
+    //In Java, the convention is that the class name should match the file name for public classes
+    public static void main(String[] args) {            //main works like the mani() in C/C++
+        System.out.println("Hello, World!");          
+    }
+}
+
+
+//? The String[] args in Java's main method serves a specific purpose related to passing command-line 
+//? arguments to a Java program when it's executed from the command line or terminal
---
+++ Basic/print3.java
@@ -0,0 +1,11 @@
+/* public */ class print3{                                 // public is a excess specifier like in C++,python
+    public static void main(String[] args) {               // won't work if public is not there because the main function becomes private function
+        System.out.println("Hello, Abdulla!");         //system is a class which has out and it is also a class which has println a function
+    }
+    String x= new String();                        //property of class or member variable of a class
+    class hi{                                       //example nested class
+
+    }                                                        
+}
+
+//indent is not important in like python  
